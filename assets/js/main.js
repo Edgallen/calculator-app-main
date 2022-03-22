e69aa60ba41950b4d2ffdd44a00c4b697c870579
Edgallen <82937338+Edgallen@users.noreply.github.com>
2022-03-22 14:20:24 +0000

    Added template for script Fixed button animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,62 @@ document.onreadystatechange = () => {
     body[0].classList.remove('preload');
   }
 };
+
+/*=============== CALCULATOR ===============*/
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    // this.currentOperand = '';
+    // this.previousOperand = '';
+    // this.operation = undefined;
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    // this.currentOperand = this.currentOperand.toSrting() + number.toString();
+    this.currentOperand = number;
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  getDisplayNumber(number) {
+
+  }
+
+  updateDispay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll('.calculator__body-number');
+const operationButtons = document.querySelectorAll('.calculator__body-operation');
+const equalsButton = document.querySelector('.calculator__body-equals');
+const deleteButton = document.querySelector('.calculator__body-delete');
+const resetButton = document.querySelector('.calculator__body-reset');
+const currentOperandTextElement = document.querySelector('.calculator__currentOperand');
+const previousOperandTextElement = document.querySelector('.calculator__previousOperand');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDispay();
+  })
+})
+
+console.log(currentOperandTextElement.innerText);
