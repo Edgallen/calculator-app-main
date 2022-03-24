9cec85a763ecabfbd790bb0ea2493cca72b6f275
Edgallen <82937338+Edgallen@users.noreply.github.com>
2022-03-24 14:29:17 +0000

    Fixed vscode bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,22 +14,6 @@ class Calculator {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
     this.clear();
-/*=============== CHEK IF PAGE LOADED ===============*/ 
-const body = document.getElementsByTagName('body');
-
-// Rome 'transition - none' when page is loaded
-document.onreadystatechange = () => {
-  if (document.readyState === 'complete') {
-    body[0].classList.remove('preload');
-  }
-};
-
-/*=============== CALCULATOR ===============*/
-class Calculator {
-  constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.previousOperandTextElement = previousOperandTextElement;
-    this.currentOperandTextElement = currentOperandTextElement;
-    this.clear();
   }
 
   clear() {
